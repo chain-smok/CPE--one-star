e5bcefe9e2e7eed2e08a260cb2db0f7e66954efc
chain-smok <101109105+chain-smok@users.noreply.github.com>
2022-05-18 09:36:25 +0800

    補充說明COLA

--- cola.c
+++ cola.c
@@ -1,17 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
 int main() {
 	int cola,count;
 	while(scanf("%d",&cola)==1){
 		count=0;
 		while(cola>=3){
-			count+=3;
-			cola-=2;
+			count+=3;//increase 3 empty bottles
+			cola-=2;//3 empty bottles exchange 1 new cola≡decrease 2 cola
 		}
-       if(cola==2)count+=3;	
-	   else count+=cola;
+       if(cola==2)count+=3;//lend one empty bottle≡increase 3 empty bottles
+	   else count+=cola;//cola=1
 	   printf("%d\n",count);
 	}
 	return 0;
